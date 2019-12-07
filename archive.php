34626066f9f1dd54109da63dbc2e53f4ab4ba258
Steve Rudolfi <sr4136@gmail.com>
2019-12-07 14:53:16 -0500

    archive cleanup

--- archive.php
+++ archive.php
@@ -16,27 +16,18 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				//the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
+				<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
 			</header><!-- .page-header -->
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				while ( have_posts() ) :
+					the_post();
 
-			endwhile;
+					get_template_part( 'template-parts/content', get_post_type() );
+				endwhile;
 
-			the_posts_navigation();
+				the_posts_navigation();
 
 		else :
 
