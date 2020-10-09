18ff12af9ee287ca1230b1aa930ce9385dc7a28e
Steve Rudolfi <srudolfi@bu.edu>
2020-10-09 11:49:54 -0400

    Gallery: showing caption, when available.

--- js/fancybox.js
+++ js/fancybox.js
@@ -5,8 +5,12 @@
 (function($){
 
 	$().fancybox( {
-	    selector : '.wp-block-gallery a',
-	    loop : true
+		selector:	'.wp-block-gallery a',
+		loop:		true,
+		caption:	function( instance, item ) {
+						var caption = $(this).siblings('figcaption').text() || '';
+						return caption;
+					},
 	} );
 
 } )( jQuery );
