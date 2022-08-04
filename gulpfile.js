819e396d34cbc2f831a29555dc576ecd18fa4a00
Steve Rudolfi <sr4136@gmail.com>
2022-08-04 17:43:42 -0400

    gulp cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,26 @@ var sass = require('gulp-sass');
 
 sass.compiler = require('node-sass');
 
-
 gulp.task( 'sass:main', function () {
     return gulp.src( './sass/style.scss' )
         .pipe( sass().on( 'error', sass.logError ) )
         .pipe( gulp.dest( './' ) );
 } );
+
 gulp.task( 'sass:blocks', function () {
     return gulp.src( './sass/blocks.scss' )
         .pipe( sass().on( 'error', sass.logError ) )
         .pipe( gulp.dest( './css/' ) );
 } );
+
 gulp.task( 'sass:admin', function () {
     return gulp.src( './sass/admin.scss' )
-        .pipe( sass().on( 'error', sass.logError ) )
+		.pipe( sass().on( 'error', sass.logError ) )
         .pipe( gulp.dest( './css/' ) );
 } );
 
-
-gulp.task( 'default', function() {
+gulp.task( 'watch', function() {
     gulp.watch('./sass/**/*.scss', gulp.series( [ 'sass:main', 'sass:blocks', 'sass:admin'] ) );
 } );
+
+gulp.task( 'default', gulp.series( [ 'sass:main', 'sass:blocks', 'sass:admin'] ) );
