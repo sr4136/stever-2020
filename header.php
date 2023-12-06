5771ab98c281306020a78aae16f9dc92bade3af0
sr4136 <sr4136@gmail.com>
2023-12-06 17:26:33 -0500

    styling: header cleanup

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -11,8 +12,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -20,37 +22,35 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'stever' ); ?></a>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'stever'); ?></a>
 
-	<header id="masthead">
-		<div id="site-branding" class="grid-container">
-            <a href="<?php echo home_url(); ?>" class="custom-logo-link" rel="home">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" class="custom-logo" alt="Steve Rudolfi Logo">
-            </a>
-		</div>
+		<header id="masthead" class="grid-container">
+			<div id="site-branding">
+				<a href="<?php echo home_url(); ?>" class="custom-logo-link" rel="home">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" class="custom-logo" alt="Steve Rudolfi Logo">
+				</a>
+			</div>
 
-		<nav id="social-navigation">
-			<?php
-			wp_nav_menu( array(
-				'theme_location'	=> 'social-icons',
-				'menu_id'			=> 'social-icons',
-				'depth'				=> 1,
-				'menu_class'		=> 'menu align-right grid-container',
-			) );
-			?>
-		</nav>
+			<nav id="social-navigation">
+				<?php
+				wp_nav_menu(array(
+					'theme_location'	=> 'social-icons',
+					'menu_id'			=> 'social-icons',
+					'depth'				=> 1,
+				));
+				?>
+			</nav>
 
-		<nav id="site-navigation">
-			<?php
-			wp_nav_menu( array(
-				'theme_location'	=> 'primary',
-				'menu_id'			=> 'primary-menu',
-				'depth'				=> 1,
-				'menu_class'		=> 'menu align-right grid-container',
-			) );
-			?>
-		</nav>
-	</header>
+			<nav id="site-navigation">
+				<?php
+				wp_nav_menu(array(
+					'theme_location'	=> 'primary',
+					'menu_id'			=> 'primary-menu',
+					'depth'				=> 1,
+				));
+				?>
+			</nav>
+		</header>
 
-	<div id="content" class="site-content grid-container">
+		<div id="content" class="site-content grid-container">
