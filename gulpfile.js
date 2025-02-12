228f79f9830bbfd552a1a4e01566765efd055f00
sr4136 <sr4136@gmail.com>
2025-02-12 18:59:11 -0500

    prodev legend styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,59 @@
-'use strict';
+"use strict";
 
-var gulp        = require( 'gulp' );
-var sass        = require( 'gulp-sass' );
-var cssNano     = require( 'gulp-cssnano' );
-var rename      = require( 'gulp-rename' );
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var cssNano = require("gulp-cssnano");
+var rename = require("gulp-rename");
 
-sass.compiler = require( 'node-sass' );
+sass.compiler = require("node-sass");
 
-gulp.task( 'sass:main', function () {
-    return gulp.src( './sass/style.scss', { sourcemaps: true } )
-        .pipe( sass().on( 'error', sass.logError ) )
-        .pipe( gulp.dest( './', { sourcemaps: true } ) )
-        .pipe( cssNano() )
-        .pipe( rename( {
-            extname: '.min.css'
-        } ) )
-        .pipe( gulp.dest( './', { sourcemaps: true } ) );
-} );
+gulp.task("sass:main", function () {
+  return gulp
+    .src("./sass/style.scss", { sourcemaps: true })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./", { sourcemaps: true }))
+    .pipe(cssNano())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(gulp.dest("./", { sourcemaps: true }));
+});
 
-gulp.task( 'sass:blocks', function () {
-    return gulp.src( './sass/blocks.scss', { sourcemaps: true } )
-        .pipe( sass().on( 'error', sass.logError ) )
-        .pipe( gulp.dest( './css/', { sourcemaps: true } ) )
-        .pipe( cssNano() )
-        .pipe( rename( {
-            extname: '.min.css'
-        } ) )
-        .pipe( gulp.dest( './css/', { sourcemaps: true } ) );
-} );
+gulp.task("sass:blocks", function () {
+  return gulp
+    .src("./sass/blocks.scss", { sourcemaps: true })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css/", { sourcemaps: true }))
+    .pipe(cssNano())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(gulp.dest("./css/", { sourcemaps: true }));
+});
 
-gulp.task( 'sass:admin', function () {
-    return gulp.src( './sass/admin.scss', { sourcemaps: true } )
-		.pipe( sass().on( 'error', sass.logError ) )
-        .pipe( gulp.dest( './css/', { sourcemaps: true } ) )
-        .pipe( cssNano() )
-        .pipe( rename( {
-            extname: '.min.css'
-        } ) )
-        .pipe( gulp.dest( './css/', { sourcemaps: true } ) );
-} );
+gulp.task("sass:admin", function () {
+  return gulp
+    .src("./sass/admin.scss", { sourcemaps: true })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css/", { sourcemaps: true }))
+    .pipe(cssNano())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(gulp.dest("./css/", { sourcemaps: true }));
+});
 
-gulp.task( 'watch', function() {
-    gulp.watch('./sass/**/*.scss', gulp.series( [ 'sass:main', 'sass:blocks', 'sass:admin'] ) );
-} );
+gulp.task("watch", function () {
+  gulp.watch(
+    "default",
+    gulp.series(["sass:main", "sass:blocks", "sass:admin"])
+  );
+});
 
-gulp.task( 'default', gulp.series( [ 'sass:main', 'sass:blocks', 'sass:admin' ] ) );
+gulp.task("default", gulp.series(["sass:main", "sass:blocks", "sass:admin"]));
