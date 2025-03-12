9321cf0dc0f95c07cf23ac7bf99a617c71b656c4
sr4136 <sr4136@gmail.com>
2025-03-12 16:11:47 -0400

    cleanup

--- inc/siteground-clear-cache.php
+++ inc/siteground-clear-cache.php
@@ -18,9 +18,10 @@ function find_last_modified_filetime($dir) {
 
 		// Construct the full path of the file or directory
 		$fullPath = "$dir/$value";
+		$fullPath = preg_replace('/\?v=[\d]+$/', '', $fullPath);
 
 		// Check if the current item is a file
-		if (is_file($fullPath)) {
+		if (file_exists($fullPath) &&is_file($fullPath)) {
 			$fileMtime = filemtime($fullPath); // Get the last modified time of the file
 			// Update lastModifiedTime if the current file is more recent
 			if ($fileMtime > $lastModifiedTime) {
@@ -32,10 +33,12 @@ function find_last_modified_filetime($dir) {
 		// If it's a directory, recursively search for modified files within it
 		$subDirLastModifiedFile = find_last_modified_filetime($fullPath);
 		if ($subDirLastModifiedFile) {
-			$subDirMtime = filemtime($subDirLastModifiedFile); // Get the last modified time of the found file
-			// Update lastModifiedTime if the found file is more recent
-			if ($subDirMtime > $lastModifiedTime) {
-				$lastModifiedTime = $subDirMtime;
+			if (file_exists($subDirLastModifiedFile) && is_file($subDirLastModifiedFile)) {
+				$subDirMtime = filemtime($subDirLastModifiedFile); // Get the last modified time of the found file
+				// Update lastModifiedTime if the found file is more recent
+				if ($subDirMtime > $lastModifiedTime) {
+					$lastModifiedTime = $subDirMtime;
+				}
 			}
 		}
 	}
@@ -56,7 +59,7 @@ function check_theme_files_change() {
 
 		// Check if the transient is false or if the new last modified time is greater than the stored one
 		if ($theme_last_modified === false || $check_for_newest > $theme_last_modified) {
-			//echo '<script type="text/javascript">alert("Purged");</script>';
+			// echo '<script type="text/javascript">alert("Purged");</script>';
 			// Clear the cache if files have changed
 			if (function_exists('sg_cachepress_purge_cache')) {
 				sg_cachepress_purge_cache(); // Purge the cache using the specific function
