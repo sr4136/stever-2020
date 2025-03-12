be371758e37b3823bcd34d1a2d194339c3cb11a2
sr4136 <sr4136@gmail.com>
2025-03-12 12:54:51 -0400

    gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,44 @@
 "use strict";
 
+// Import required Gulp plugins
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var cssNano = require("gulp-cssnano");
 var rename = require("gulp-rename");
 
+// Initialize Sass with node-sass
 sass.compiler = require("node-sass");
 
+// Function to compile Sass files, minify CSS, and rename output
+function compileSass(src, dest) {
+  return gulp
+    .src(src, { sourcemaps: true }) // Source files with sourcemaps
+    .pipe(sass().on("error", sass.logError)) // Compile Sass and log errors
+    .pipe(gulp.dest(dest, { sourcemaps: true })) // Output unminified CSS
+    .pipe(cssNano()) // Minify CSS
+    .pipe(rename({ extname: ".min.css" })) // Rename to .min.css
+    .pipe(gulp.dest(dest, { sourcemaps: true })); // Output minified CSS
+}
+
+// Task to compile main Sass file
 gulp.task("sass:main", function () {
-  return gulp
-    .src("./sass/style.scss", { sourcemaps: true })
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./", { sourcemaps: true }))
-    .pipe(cssNano())
-    .pipe(
-      rename({
-        extname: ".min.css",
-      })
-    )
-    .pipe(gulp.dest("./", { sourcemaps: true }));
+  return compileSass("./sass/style.scss", "./"); // Compile main Sass
 });
 
+// Task to compile blocks Sass file
 gulp.task("sass:blocks", function () {
-  return gulp
-    .src("./sass/blocks.scss", { sourcemaps: true })
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css/", { sourcemaps: true }))
-    .pipe(cssNano())
-    .pipe(
-      rename({
-        extname: ".min.css",
-      })
-    )
-    .pipe(gulp.dest("./css/", { sourcemaps: true }));
+  return compileSass("./sass/blocks.scss", "./css/"); // Compile blocks Sass
 });
 
+// Task to compile admin Sass file
 gulp.task("sass:admin", function () {
-  return gulp
-    .src("./sass/admin.scss", { sourcemaps: true })
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css/", { sourcemaps: true }))
-    .pipe(cssNano())
-    .pipe(
-      rename({
-        extname: ".min.css",
-      })
-    )
-    .pipe(gulp.dest("./css/", { sourcemaps: true }));
+  return compileSass("./sass/admin.scss", "./css/"); // Compile admin Sass
 });
 
+// Watch task to monitor changes in Sass files
 gulp.task("watch", function () {
-  gulp.watch(
-    "default",
-    gulp.series(["sass:main", "sass:blocks", "sass:admin"])
-  );
+  gulp.watch("./sass/**/*.scss", gulp.series("default")); // Watch all .scss files
 });
 
-gulp.task("default", gulp.series(["sass:main", "sass:blocks", "sass:admin"]));
+// Default task to run all Sass compilation tasks
+gulp.task("default", gulp.series("sass:main", "sass:blocks", "sass:admin"));
