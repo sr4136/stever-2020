b7b69864602507a749bb9e6dab1a40cb44192854
sr4136 <sr4136@gmail.com>
2025-03-31 16:04:33 -0400

    header styles to add emphasis, cleanup

--- archive-stever_quotes.php
+++ archive-stever_quotes.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -10,35 +11,35 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php if (have_posts()) : ?>
 
 			<header class="page-header">
-				<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+				<?php the_archive_title('<h1 class="page-title">', '</h1>'); ?>
 				<a class="button button-ghost" href="https://steverudolfi.com/quotes/?layout=slideshow">View as a slideshow</a>
 			</header><!-- .page-header -->
 
-			<?php
+		<?php
 
-				while ( have_posts() ) :
-					the_post();
+			while (have_posts()) :
+				the_post();
 
-					get_template_part( 'template-parts/content', get_post_type() );
-				endwhile;
+				get_template_part('template-parts/content', get_post_type());
+			endwhile;
 
-				the_posts_navigation();
+			the_posts_navigation();
 
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+			get_template_part('template-parts/content', 'none');
 
 		endif;
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
