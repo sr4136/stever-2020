f2961d949d79caa2e326b4b17c1a6337ed8b4a24
sr4136 <sr4136@gmail.com>
2025-04-23 13:14:08 -0400

    swap the two googlefonts to use bunnyfonts

--- inc/enqueue-scripts-styles.php
+++ inc/enqueue-scripts-styles.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Enqueue: Scripts & Styles - Frontend & Admin.
  * @package stever
@@ -9,114 +10,123 @@
  * Frontend Enqueue
  */
 function stever_scripts_styles() {
-	
-	// Use the "Body Class" metabox as a vehicle to enqueue scripts/styles.
-	$stever_body_class_text = get_post_meta( get_the_id(), 'stever_body_class_text', true );
 
-	/* Fonts */
-    // Body.
-    wp_enqueue_style( 'googleFonts-Roboto', '//fonts.googleapis.com/css?family=Roboto:400,700,400italic,700italic' );
-    // Headings.
-    wp_enqueue_style( 'googleFonts-Lato', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' );
-    // Mono.
-    wp_enqueue_style( 'bunnyFonts-B612', '//fonts.bunny.net/css?family=b612-mono:400' );
-    // Icons.
-    wp_enqueue_style( 'fontawesome',
-        get_stylesheet_directory_uri() . '/css/fontawesome-free-5.8.1-web/css/all.min.css',
-        array(),
-        filemtime( get_stylesheet_directory() . '/css/fontawesome-free-5.8.1-web/css/all.min.css' )
-    );
+	// Use the "Body Class" metabox as a vehicle to enqueue scripts/styles.
+	$stever_body_class_text = get_post_meta(get_the_id(), 'stever_body_class_text', true);
+
+	// Fonts.
+	wp_enqueue_style('bunnyFonts', '//fonts.bunny.net/css?family=b612-mono:400,400i,700,700i|lato:400,400i,700,700i|roboto:400,400i,700,700i');
+
+	// Icons.
+	wp_enqueue_style(
+		'fontawesome',
+		get_stylesheet_directory_uri() . '/css/fontawesome-free-5.8.1-web/css/all.min.css',
+		array(),
+		filemtime(get_stylesheet_directory() . '/css/fontawesome-free-5.8.1-web/css/all.min.css')
+	);
 
 	// CSS: Underscores.
-	wp_enqueue_style( 'stever-underscores',
+	wp_enqueue_style(
+		'stever-underscores',
 		get_stylesheet_directory_uri() . '/css/underscores.css',
 		array(),
-		filemtime( get_stylesheet_directory() . '/css/underscores.css' )
+		filemtime(get_stylesheet_directory() . '/css/underscores.css')
 	);
 
 	// CSS: Foundation.
-	wp_enqueue_style( 'stever-foundation',
+	wp_enqueue_style(
+		'stever-foundation',
 		get_stylesheet_directory_uri() . '/css/foundation.css',
 		array(),
-		filemtime( get_stylesheet_directory() . '/css/foundation.css' )
+		filemtime(get_stylesheet_directory() . '/css/foundation.css')
 	);
 
 	// CSS: SteveR Theme Styles. 
-	wp_enqueue_style( 'stever-style',
+	wp_enqueue_style(
+		'stever-style',
 		get_stylesheet_directory_uri() . '/style.min.css',
 		array(),
-		filemtime( get_stylesheet_directory() . '/style.min.css' )
+		filemtime(get_stylesheet_directory() . '/style.min.css')
 	);
 
 	// CSS: SteveR Blocks Styles.
-	wp_enqueue_style( 'stever-blocks-front',
+	wp_enqueue_style(
+		'stever-blocks-front',
 		get_stylesheet_directory_uri() . '/css/blocks.min.css',
 		array(),
-		filemtime( get_stylesheet_directory() . '/css/blocks.min.css' )
+		filemtime(get_stylesheet_directory() . '/css/blocks.min.css')
 	);
-	
+
 	// JS: Fix "skip-link focus".
-	wp_enqueue_script( 'stever-skip-link-focus-fix',
-        get_template_directory_uri() . '/js/skip-link-focus-fix.js',
-        array(),
-        filemtime( get_stylesheet_directory() . '/js/skip-link-focus-fix.js' ),
-        true
-    );
+	wp_enqueue_script(
+		'stever-skip-link-focus-fix',
+		get_template_directory_uri() . '/js/skip-link-focus-fix.js',
+		array(),
+		filemtime(get_stylesheet_directory() . '/js/skip-link-focus-fix.js'),
+		true
+	);
 
-    // CSS & JS: Tipped.
-    if (
-        strpos( $stever_body_class_text, 'sr-page-table-prodev' ) !== false ||
-        strpos( $stever_body_class_text, 'sr-bu-timeline' ) !== false ||
-        strpos($stever_body_class_text, 'sr-page-table-books') !== false
-    ) {
-        wp_enqueue_style( 'stever-tipped',
-            get_stylesheet_directory_uri() . '/js/tipped/tipped.css',
-            array(),
-            filemtime( get_stylesheet_directory() . '/js/tipped/tipped.css' )
-        );
-        wp_enqueue_script( 'stever-tipped',
-            get_template_directory_uri() . '/js/tipped/tipped.min.js',
-            array(),
-            filemtime( get_stylesheet_directory() . '/js/tipped/tipped.min.js' ),
-            true
-        );
-        wp_enqueue_script( 'stever-tipped-kickoff',
-            get_template_directory_uri() . '/js/tipped/tipped-kickoff.js',
-            array(),
-            filemtime( get_stylesheet_directory() . '/js/tipped/tipped-kickoff.js' ),
-            true
-        );
-        wp_enqueue_script( 'stever-dragscroll',
-            get_template_directory_uri() . '/js/dragscroll/dragscroll.js',
-            array(),
-            filemtime( get_stylesheet_directory() . '/js/dragscroll/dragscroll.js' ),
-            true
-        );
-    }
+	// CSS & JS: Tipped.
+	if (
+		strpos($stever_body_class_text, 'sr-page-table-prodev') !== false ||
+		strpos($stever_body_class_text, 'sr-bu-timeline') !== false ||
+		strpos($stever_body_class_text, 'sr-page-table-books') !== false
+	) {
+		wp_enqueue_style(
+			'stever-tipped',
+			get_stylesheet_directory_uri() . '/js/tipped/tipped.css',
+			array(),
+			filemtime(get_stylesheet_directory() . '/js/tipped/tipped.css')
+		);
+		wp_enqueue_script(
+			'stever-tipped',
+			get_template_directory_uri() . '/js/tipped/tipped.min.js',
+			array(),
+			filemtime(get_stylesheet_directory() . '/js/tipped/tipped.min.js'),
+			true
+		);
+		wp_enqueue_script(
+			'stever-tipped-kickoff',
+			get_template_directory_uri() . '/js/tipped/tipped-kickoff.js',
+			array(),
+			filemtime(get_stylesheet_directory() . '/js/tipped/tipped-kickoff.js'),
+			true
+		);
+		wp_enqueue_script(
+			'stever-dragscroll',
+			get_template_directory_uri() . '/js/dragscroll/dragscroll.js',
+			array(),
+			filemtime(get_stylesheet_directory() . '/js/dragscroll/dragscroll.js'),
+			true
+		);
+	}
 
-    // CSS & JS: Fancybox (for Galleries).
-    $has_gallery = has_block( 'gallery', get_the_id() );
-	if ( $has_gallery ) {
-		wp_enqueue_style( 'stever-fancybox',
-            get_stylesheet_directory_uri() . '/js/fancybox/jquery.fancybox.min.css',
-            array(),
-            filemtime( get_stylesheet_directory() . '/js/fancybox/jquery.fancybox.min.css' )
-        );
-        wp_enqueue_script( 'stever-fancybox',
-            get_template_directory_uri() . '/js/fancybox/jquery.fancybox.min.js',
-            array( 'jquery' ),
-            filemtime( get_stylesheet_directory() . '/js/fancybox/jquery.fancybox.min.js' ),
-            true
-        );
-        wp_enqueue_script( 'stever-fancybox-init',
-            get_template_directory_uri() . '/js/fancybox.js',
-            array( 'jquery', 'stever-fancybox' ),
-            filemtime( get_stylesheet_directory() . '/js/fancybox.js' ),
-            true
-        );
+	// CSS & JS: Fancybox (for Galleries).
+	$has_gallery = has_block('gallery', get_the_id());
+	if ($has_gallery) {
+		wp_enqueue_style(
+			'stever-fancybox',
+			get_stylesheet_directory_uri() . '/js/fancybox/jquery.fancybox.min.css',
+			array(),
+			filemtime(get_stylesheet_directory() . '/js/fancybox/jquery.fancybox.min.css')
+		);
+		wp_enqueue_script(
+			'stever-fancybox',
+			get_template_directory_uri() . '/js/fancybox/jquery.fancybox.min.js',
+			array('jquery'),
+			filemtime(get_stylesheet_directory() . '/js/fancybox/jquery.fancybox.min.js'),
+			true
+		);
+		wp_enqueue_script(
+			'stever-fancybox-init',
+			get_template_directory_uri() . '/js/fancybox.js',
+			array('jquery', 'stever-fancybox'),
+			filemtime(get_stylesheet_directory() . '/js/fancybox.js'),
+			true
+		);
 	}
 }
-add_action( 'wp_enqueue_scripts', 'stever_scripts_styles' );
+add_action('wp_enqueue_scripts', 'stever_scripts_styles');
 
 
 /**
@@ -124,10 +134,11 @@ add_action( 'wp_enqueue_scripts', 'stever_scripts_styles' );
  */
 function stever_admin_scripts_styles() {
 	// CSS: Admin General.
-	wp_enqueue_style( 'stever-admin',
+	wp_enqueue_style(
+		'stever-admin',
 		get_stylesheet_directory_uri() . '/css/admin.min.css',
 		array(),
-		filemtime( get_stylesheet_directory() . '/css/admin.min.css' )
+		filemtime(get_stylesheet_directory() . '/css/admin.min.css')
 	);
 
 	// Icons.
@@ -138,7 +149,7 @@ function stever_admin_scripts_styles() {
 		filemtime(get_stylesheet_directory() . '/css/fontawesome-free-5.8.1-web/css/all.min.css')
 	);
 }
-add_action( 'admin_enqueue_scripts', 'stever_admin_scripts_styles' );
+add_action('admin_enqueue_scripts', 'stever_admin_scripts_styles');
 
 /**
  * Block admin
@@ -168,8 +179,8 @@ add_action('enqueue_block_assets', 'stever_block_styles');
  * Custom Favicon for Admin & Login.
  */
 function stever_admin_favicon() {
-  	$favicon_url = get_stylesheet_directory_uri() . '/img/favicon-admin.ico';
-	echo( '<link rel="shortcut icon" href="' . $favicon_url . '" />' );
+	$favicon_url = get_stylesheet_directory_uri() . '/img/favicon-admin.ico';
+	echo ('<link rel="shortcut icon" href="' . $favicon_url . '?' . uniqid() . '" />');
 }
-add_action( 'login_head', 'stever_admin_favicon' );
-add_action( 'admin_head', 'stever_admin_favicon' );
+add_action('login_head', 'stever_admin_favicon');
+add_action('admin_head', 'stever_admin_favicon');
