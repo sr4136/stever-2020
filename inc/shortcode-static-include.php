bfe041331e7849607ed46c8c2ba4c54741e88479
sr4136 <sr4136@gmail.com>
2026-01-01 14:29:29 -0500

    static file include menthod change
    from file_get_contents to WP_Filesystem_Direct

--- inc/shortcode-static-include.php
+++ inc/shortcode-static-include.php
@@ -1,11 +1,13 @@
 <?php
 /* Static File */
-function stever_static_shortcode( $atts ) {
+function stever_static_shortcode($atts) {
 	$file = $atts['file'];
 	$fullPath = get_stylesheet_directory_uri() . '/static/' . $file;
 
-	$fileContent = file_get_contents( $fullPath );
+	$filesystem = new WP_Filesystem_Direct(true);
+	$fileContent = $filesystem->get_contents($fullPath);
+
 
 	return $fileContent;
 }
-add_shortcode( 'static', 'stever_static_shortcode' );
+add_shortcode('static', 'stever_static_shortcode');
